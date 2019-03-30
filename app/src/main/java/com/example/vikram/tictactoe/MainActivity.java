bd6bf1ebb89faf52a64ea6c8907d6518027b2a41
Vikram <vikramkumaresan@rocketmail.com>
2019-03-30 14:50:26 +0530

    Add main activity

---
+++ app/src/main/java/com/example/vikram/tictactoe/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.vikram.tictactoe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToGameActivity(View view){
+        Intent intent = new Intent(this,GameActivity.class);
+        startActivity(intent);
+    }
+}
